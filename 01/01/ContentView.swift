f04e307de778de05dc3a46c5a81e4abb46bf066a
haoboxuxu <haoboxuxu@icloud.com>
2020-04-04 19:00:04 +0800

    update

--- 01/01/ContentView.swift
+++ 01/01/ContentView.swift
@@ -12,44 +12,63 @@ struct ContentView: View {
     
     @State var show = false
     @State var viewState = CGSize.zero
+    @State var showCard = false
     
     var body: some View {
         ZStack {
             
             TittleView()
                 .blur(radius: show ? 20 : 0)
-                .animation(.default)
+                .opacity(showCard ? 0.4 : 1)
+                .offset(y: showCard ? -200 : 0)
+                .animation(
+                    Animation
+                        .default
+                        .delay(0.1)
+                )
             
             BackCardView()
+                .frame(width: showCard ? 300 : 340, height: 220)
                 .background(show ? Color("card3") : Color("card4"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -400 : -40)
                 .offset(x: viewState.width, y : viewState.height)
-                .scaleEffect(0.9)
+                .offset(y: showCard ? -180 : 0)
+                .scaleEffect(showCard ? 1 : 0.9)
                 .rotationEffect(Angle(degrees: show ? 0 : 10))
-                .rotation3DEffect(Angle(degrees: 10), axis: (x: 10, y: 0, z: 0))
+                .rotationEffect(Angle(degrees: showCard ? -10 : 0))
+                .rotation3DEffect(Angle(degrees: showCard ? 0 : 10), axis: (x: 10, y: 0, z: 0))
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.5))
             
             BackCardView()
+                .frame(width: 340, height: 220)
                 .background(show ? Color("card4") : Color("card3"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -200 : -20)
                 .offset(x: viewState.width, y : viewState.height)
-                .scaleEffect(0.95)
+                .offset(y: showCard ? -140 : 0)
+                .scaleEffect(showCard ? 1 : 0.95)
                 .rotationEffect(Angle(degrees: show ? 0 : 5))
-                .rotation3DEffect(Angle(degrees: 5), axis: (x: 10, y: 0, z: 0))
+                .rotationEffect(Angle(degrees: showCard ? -5 : 0))
+                .rotation3DEffect(Angle(degrees: showCard ? 0 : 5), axis: (x: 10, y: 0, z: 0))
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.3))
             
             CardView()
-                .offset(x: viewState.width, y : viewState.height)
+                .frame(width: showCard ? 375 : 340, height: 220)
+                .background(Color.black)
+                //.cornerRadius(20)
+                .clipShape(RoundedRectangle(cornerRadius: showCard ? 30 : 20, style: .continuous))
+                .shadow(radius: 20)
+                .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -100 : 0)
                 .blendMode(.hardLight)
                 .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
                 .onTapGesture {
-                    self.show.toggle()
+                    self.showCard.toggle()
             }
             .gesture(
                 DragGesture().onChanged { value in
@@ -64,8 +83,9 @@ struct ContentView: View {
             
             
             ButtomCardView()
+                .offset(x: 0, y: showCard ? 360 : 1000)
                 .blur(radius: show ? 20 : 0)
-                .animation(.default)
+                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
         }
         
     }
@@ -99,10 +119,7 @@ struct CardView: View {
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 300, height: 110, alignment: .top)
         }
-        .frame(width: 340, height: 220)
-        .background(Color.black)
-        .cornerRadius(20)
-        .shadow(radius: 20)
+        
     }
 }
 
@@ -111,7 +128,6 @@ struct BackCardView: View {
         VStack {
             Spacer()
         }
-        .frame(width: 340, height: 220)
     }
 }
 
@@ -150,6 +166,5 @@ struct ButtomCardView: View {
         .background(Color.white)
         .cornerRadius(30)
         .shadow(radius: 20)
-        .offset(x: 0, y: 500)
     }
 }
