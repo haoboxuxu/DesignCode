547323041609bb70451042da0166112fdfd9dd7e
haoboxuxu <haoboxuxu@icloud.com>
2020-04-05 23:38:49 +0800

    update

--- 01/01/HomeView.swift
+++ 01/01/HomeView.swift
@@ -19,7 +19,19 @@ struct HomeView: View {
                 MeView(showProfile: $showProfile)
             }
             .padding(.horizontal)
+            .padding(.leading, 14)
             .padding(.top, 30)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30) {
+                    ForEach(0 ..< 5) { item in
+                        SctionView()
+                    }
+                }
+                .padding(30)
+                .padding(.bottom, 30)
+            }
+            
             Spacer()
         }
     }
@@ -28,5 +40,32 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView(showProfile: .constant(false))
+    }
+}
+
+struct SctionView: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Text("Prototype design in SwiftUI")
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                Spacer()
+                Image("Logo1")
+            }
+            Text("18 Section".uppercased())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width:210)
+        }
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .frame(width: 275, height: 275)
+        .background(Color("card1"))
+        .cornerRadius(30)
+        .shadow(color: Color("card1").opacity(0.3), radius: 20, x: 0, y: 20)
     }
 }
