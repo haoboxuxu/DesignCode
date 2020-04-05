984353ed9cd9865ef18402b6bdac0a2ff7ebe1e8
haoboxuxu <haoboxuxu@icloud.com>
2020-04-05 18:33:53 +0800

    update

--- 01/01/MenuView.swift
+++ 01/01/MenuView.swift
@@ -15,16 +15,38 @@ struct MenuView: View {
             Spacer()
             VStack(spacing: 16) {
                 
+                Text("haoboxuxu  - 28%")
+                    .font(.caption)
+                
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .cornerRadius(3)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.08))
+                    .cornerRadius(3)
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(12)
+                
                 MenuRow(title: "Account", icon: "gear")
                 MenuRow(title: "Billing", icon: "creditcard")
                 MenuRow(title: "Sign out", icon: "person.crop.circle")
             }
             .frame(maxWidth: .infinity)
             .frame(height: 300)
-            .background(Color.white)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), Color(#colorLiteral(red: 0.8705882353, green: 0.8941176471, blue: 0.9450980392, alpha: 1))]), startPoint: .top, endPoint: .bottom))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(radius: 30)
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .padding(.horizontal, 30)
+            .overlay (
+                Image("haoboxuxu")
+                .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 60, height: 60)
+                .clipShape(Circle())
+                    .offset(y: -150)
+            )
         }
         .padding(.bottom, 30)
     }
@@ -46,6 +68,8 @@ struct MenuRow: View {
                 .font(.system(size: 20, weight: .light, design: .rounded))
                 .imageScale(.large)
                 .frame(width: 32, height: 32)
+                .foregroundColor(Color(#colorLiteral(red: 0.662745098, green: 0.7333333333, blue: 0.831372549, alpha: 1)))
+            
             Text(title)
                 .font(.system(size: 20, weight: .bold, design: .rounded))
                 .frame(width: 120, alignment: .leading)
