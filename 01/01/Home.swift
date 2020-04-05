a79d38abb6bd5808ec5a5c0aaaeb71685dd0d4c4
haoboxuxu <haoboxuxu@icloud.com>
2020-04-05 19:04:42 +0800

    update

--- 01/01/Home.swift
+++ 01/01/Home.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct Home: View {
     
     @State var showProfile = false
+    @State var viewState = CGSize.zero
     
     var body: some View {
         ZStack {
@@ -40,14 +41,30 @@ struct Home: View {
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .offset(y: showProfile ? -450 : 0)
-            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10, y: 0, z: 0))
+            .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10, y: 0, z: 0))
             .scaleEffect(showProfile ? 0.9 : 1)
             .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
             .edgesIgnoringSafeArea(.all)
             
             MenuView()
+                .background(Color.black.opacity(0.001))
                 .offset(y: showProfile ? 0 : 600)
+                .offset(y: viewState.height)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    self.showProfile.toggle()
+            }
+            .gesture(
+                DragGesture().onChanged { value in
+                    self.viewState = value.translation
+                }
+                .onEnded { value in
+                    if self.viewState.height > 50 {
+                        self.showProfile = false
+                    }
+                    self.viewState = .zero
+                }
+            )
         }
     }
 }
